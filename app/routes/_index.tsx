1c090150b2c1747a88361047524370d6735091da
victormsz09 <victormsz09@yahoo.com.br>
2025-09-15 14:43:22 -0300

    chore: update dependencies in package.json
    - Added @react-router/fs-routes version 7.9.1
    - Updated react-router from version 7.7.1 to 7.8.2

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+
+export default function HomePage() {
+    return (
+        <div style={{ textAlign: "center", padding: "2rem" }}>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the starting point of your React Router app.</p>
+            <nav>
+            </nav>
+        </div>
+    );
+}
