8ac55c56e6138134c6383b3631061bbcc4c15042
Victor Z <victormsz09@yahoo.com.br>
2025-09-22 19:17:13 -0300

    feat: enhance Card component to include href prop and update flight display logic

--- app/components/swift-route/Card.tsx
+++ app/components/swift-route/Card.tsx
@@ -11,36 +11,39 @@ interface CardProps {
   type: 'flight' | 'blog' | 'transfer';
   title?: string;
   flightProps?: FlightProps;
+  href?: string;
 }
 
-export const Card = ({ type, image, title, flightProps }: CardProps) => {
+export const Card = ({ type, image, title, flightProps, href }: CardProps) => {
   if (type === 'flight' && flightProps) {
     const { cost, currency = 'US$', from, destination, isPromo } = flightProps;
     return (
-      <div className="bg-white rounded-2xl shadow-2xs overflow-hidden w-80">
-        {/* Imagem da promo */}
-        <div className="relative">
-          <img src={image} alt="" className="w-full h-48 object-cover" />
-          {isPromo && (
-            <span className="absolute top-3 left-3 bg-red-600 text-white text-sm font-semibold px-3 py-1 rounded-full shadow">
-              Unmissable Offer
-            </span>
-          )}
-        </div>
-
-        {/* Content */}
-        <div className="p-5 space-y-2 text-text-main-swift">
-          <h3 className="text-lg font-semibold">Flight to {destination}</h3>
-          <p className="text-gray-500 text-sm">
-            Leaving {from}
-            <br />
-            Round Trip
-          </p>
-          <div className="flex items-baseline gap-1">
-            <span className=" text-sm">{currency}</span>
-            <span className=" text-2xl font-bold">{cost}</span>
+      <div className="bg-white rounded-2xl overflow-hidden w-80 shadow-lg hover:shadow-2xl transition-shadow duration-300 hover:cursor-pointer">
+        <a href={href}>
+          <div className="relative">
+            <img src={image} alt="" className="w-full h-48 object-cover" />
+            {isPromo && (
+              <span className="absolute top-3 left-3 bg-red-600 text-white text-sm font-semibold px-3 py-1 rounded-full shadow">
+                Unmissable Offer
+              </span>
+            )}
           </div>
-        </div>
+
+          <div className="p-5 flex justify-between items-end text-text-main-swift">
+            <div className="space-y-1">
+              <h3 className="text-lg font-semibold">Flight to {destination}</h3>
+              <p className="text-gray-500 text-sm">
+                Leaving {from}
+                <br />
+                Round Trip
+              </p>
+            </div>
+            <div className="flex items-baseline gap-1">
+              <span className="text-sm">{currency}</span>
+              <span className="text-2xl font-bold">{cost}</span>
+            </div>
+          </div>
+        </a>
       </div>
     );
   }
--- app/routes/swift-route./route.tsx
+++ app/routes/swift-route./route.tsx
@@ -1,18 +1,75 @@
 import { Card } from '../../components/swift-route/Card';
+import image from 'app/routes/swift-route./flight.jpeg';
+const flights = [
+  {
+    id: 1,
+    from: 'New York',
+    destination: 'London',
+    cost: 199,
+    currency: 'US$',
+    isPromo: true,
+    image: '/flight.jpeg',
+  },
+  {
+    id: 2,
+    from: 'Los Angeles',
+    destination: 'Tokyo',
+    cost: 299,
+    currency: 'US$',
+    isPromo: false,
+    image: '/flight.jpeg',
+  },
+  {
+    id: 3,
+    from: 'Chicago',
+    destination: 'Paris',
+    cost: 249,
+    currency: 'US$',
+    isPromo: true,
+    image: '/flight.jpeg',
+  },
+  {
+    id: 4,
+    from: 'Miami',
+    destination: 'Rome',
+    cost: 279,
+    currency: 'US$',
+    isPromo: false,
+    image: '/flight.jpeg',
+  },
+  {
+    id: 5,
+    from: 'San Francisco',
+    destination: 'Berlin',
+    cost: 319,
+    currency: 'US$',
+    isPromo: true,
+    image: '/flight.jpeg',
+  },
+];
 export default function SwiftRoute() {
   return (
     <>
       <div className=" bg-white">
-        <Card
-          type="flight"
-          image="/eiffel-tower-sunrise1.jpg.webp"
-          flightProps={{
-            cost: 199,
-            from: 'New York',
-            destination: 'London',
-            isPromo: true,
-          }}
-        />
+        <h1 className="text-3xl font-bold mb-6 text-center text-text-main-swift pt-10">
+          Available Flights
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center pb-10">
+          {flights.map((flight) => (
+            <Card
+              key={flight.id}
+              type="flight"
+              image={image}
+              flightProps={{
+                cost: flight.cost,
+                currency: flight.currency,
+                from: flight.from,
+                destination: flight.destination,
+                isPromo: flight.isPromo,
+              }}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
