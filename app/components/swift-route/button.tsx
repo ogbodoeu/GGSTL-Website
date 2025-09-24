2d2d7e783c06a39dbaa915454afec17ee62d9463
Victor Z <victormsz09@yahoo.com.br>
2025-09-24 14:27:29 -0300

    feat: add ButtonSwift component with clear and colored styles

---
+++ app/components/swift-route/button.tsx
@@ -0,0 +1,34 @@
+interface ButtonSwiftProps {
+  onClick?: () => void;
+  BgType: 'clear' | 'colored';
+  children?: React.ReactNode;
+}
+export default function ButtonSwift({ onClick, children, BgType }: ButtonSwiftProps) {
+  if (BgType === 'clear') {
+    return (
+      <button
+        className="h-20 w-40 px-4 py-2 rounded-full border-2 bg-inherit border-btn-secondary-swift text-text-main-swift font-medium transition-colors"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  } else if (BgType === 'colored') {
+    return (
+      <button
+        className="h-20 w-40 px-4 py-2 rounded-full border-2 bg-btn-secondary-swift text-white font-semibold text-sm hover:bg-pink-700 transition-colors"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  }
+}
+// width: 153;
+// height: 82;
+// angle: 0 deg;
+// opacity: 1;
+// top: 82px;
+// left: 1532px;
+// border-radius: 50px;
+// border-width: 2px;
