8f647930a258f72dc3cf1b9d9319ad77e28db873
FelipeZeferino <fzeferino16@gmail.com>
2025-09-25 18:26:52 -0300

    feat(web): login page and money transfer

---
+++ app/components/swift-route/money-transfer.tsx
@@ -0,0 +1,147 @@
+import { useState } from 'react';
+
+export default function MoneyTransfer() {
+  type Currency = {
+    code: 'USD' | 'EUR' | 'GBP' | 'JPY' | 'AUD' | 'CAD' | 'BRL';
+    name: string;
+  };
+
+  const currencies: Currency[] = [
+    { code: 'USD', name: 'US Dollar' },
+    { code: 'EUR', name: 'Euro' },
+    { code: 'GBP', name: 'British Pound' },
+    { code: 'JPY', name: 'Japanese Yen' },
+    { code: 'AUD', name: 'Australian Dollar' },
+    { code: 'CAD', name: 'Canadian Dollar' },
+    { code: 'BRL', name: 'Brazilian Real' },
+  ];
+
+  const currencyFlags: Record<Currency['code'], string> = {
+    USD: '🇺🇸',
+    EUR: '🇪🇺',
+    GBP: '🇬🇧',
+    JPY: '🇯🇵',
+    AUD: '🇦🇺',
+    CAD: '🇨🇦',
+    BRL: '🇧🇷',
+  };
+
+  const [toCurrency, setToCurrency] = useState<Currency['code']>(currencies[0].code);
+  const [fromCurrency, setFromCurrency] = useState<Currency['code']>(currencies[1].code);
+  const [amountToConvert, setAmountToConvert] = useState<number | string>('');
+
+  return (
+    <div className="w-[80%] min-h-[400px] flex flex-col items-center justify-center shadow-2xl drop-shadow-2xl rounded-xl md:rounded-[90px] md:p-12">
+      <h1 className="text-md md:text-xl font-bold my-4">Make your transfer or Conversion here</h1>
+      <div className="flex flex-col gap-4">
+        <label className="flex items-center justify-between">
+          <span>From:</span>
+          <select
+            className="border-2 border-primary-pink rounded-full p-2 outline-0 ml-2 w-48"
+            value={fromCurrency}
+            onChange={(e) => setFromCurrency(e.target.value as Currency['code'])}
+          >
+            <option value="">Select currency</option>
+            {currencies.map((currency) => (
+              <option key={currency.code} value={currency.code}>
+                {currency.name} {currencyFlags[currency.code]}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label className="flex items-center justify-between">
+          <span>To:</span>
+          <select
+            className="border-2 border-primary-pink rounded-full p-2 outline-0 ml-2 w-48"
+            value={toCurrency}
+            onChange={(e) => setToCurrency(e.target.value as Currency['code'])}
+          >
+            <option value="">Select currency</option>
+            {currencies.map((currency) => (
+              <option key={currency.code} value={currency.code}>
+                {currency.name} {currencyFlags[currency.code]}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+
+      <div>
+        1 {fromCurrency} = 1.23 {toCurrency}
+      </div>
+      <div className="flex items-center flex-col md:flex-row gap-2 mt-4">
+        <input
+          className="border-2 border-primary-pink rounded-full p-2 outline-0 no-spinners"
+          type="number"
+          placeholder="Amount"
+          min={0}
+          value={amountToConvert}
+          onChange={(e) => {
+            const value = e.target.value;
+            if (Number(value) >= 0 || value === '') {
+              setAmountToConvert(value);
+            }
+          }}
+        />
+        <button className="border-2 border-primary-pink rounded-full px-4 py-2 font-semibold text-primary-pink">
+          <img src="/icons/convert.svg" alt="Convert" className="w-4 h-4" />
+        </button>
+        <input
+          className="border-2 border-primary-pink rounded-full p-2 outline-0"
+          type="text"
+          placeholder="Converted Amount"
+          readOnly
+          value={amountToConvert ? (Number(amountToConvert) * 1.23).toFixed(2) : ''}
+        />
+      </div>
+      <div className="w-[80%] border-1 border-primary-pink rounded-full my-8" />
+      {/*  bank account or store cash  */}
+      <div className="w-[90%] flex items-center justify-center flex-col">
+        <h4 className="text-md md:text-lg font-bold my-2">Select your payout method</h4>
+        <div className="flex flex-row items-center justify-center   ">
+          <div className="flex flex-col items-center">
+            <img className="h-20 m-2" src="/icons/store.svg" />
+            <button className="cursor-pointer min-w-[100px] md:min-w-[200px] min-h-[45px] md:min-h-[87px] flex text-xs md:text-[1rem] items-center justify-center font-bold border-2 border-primary-pink rounded-bl-full rounded-br-full rounded-tr-full bg-primary-pink text-white">
+              Receive cash in a <br /> store
+            </button>
+          </div>
+          <div className="flex flex-col items-center m-2">
+            <img className="h-20 m-2" src="/icons/bank.svg" />
+            <button className="cursor-pointer min-w-[100px] md:min-w-[200px] min-h-[45px] md:min-h-[87px] flex text-xs md:text-[1rem] items-center justify-center font-bold border-2 border-primary-pink rounded-bl-full rounded-br-full rounded-tr-full bg-white text-primary-pink">
+              Bank account
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="w-[80%] border-1 border-primary-pink rounded-full my-8" />
+      {/*  payment method  */}
+      <div className="w-[90%] flex items-center justify-center flex-col">
+        <h4 className="text-md md:text-lg font-bold my-2">Payment Method</h4>
+        <div className="flex flex-row items-center justify-center">
+          <div className="flex flex-col items-center">
+            <img className="h-20 m-2" src="/icons/store.svg" />
+            <button className="cursor-pointer min-w-[100px] md:min-w-[200px] min-h-[45px] md:min-h-[87px] flex text-xs md:text-[1rem] items-center justify-center font-bold border-2 border-primary-pink rounded-bl-full rounded-br-full rounded-tr-full bg-primary-pink text-white">
+              PIX or Bank <br /> account
+            </button>
+          </div>
+          <div className="flex flex-col items-center m-2">
+            <img className="h-20 m-2" src="/icons/cellphone-payment.svg" />
+            <button className="cursor-pointer min-w-[100px] md:min-w-[200px] min-h-[45px] md:min-h-[87px] flex text-xs md:text-[1rem] items-center justify-center font-bold border-2 border-primary-pink rounded-bl-full rounded-br-full rounded-tr-full bg-white text-primary-pink">
+              Online Payment
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="w-[80%] border-1 border-primary-pink rounded-full my-4" />
+        <div className=" min-w-[50%] max-w-[200px] flex flex-row items-center justify-center">
+            <img className="h-12 m-2" src="/icons/coupon.svg" />
+            <input className="rounded-full p-2 outline-0 shadow-2xl" type="text" placeholder="Promotional code" />
+        </div>
+      <div className="w-[80%] border-1 border-primary-pink rounded-full my-4" />
+        <button className="cursor-pointer min-w-[150px] md:min-w-[250px] min-h-[50px] flex text-md md:text-[1.2rem] items-center justify-center font-bold border-2 border-primary-pink rounded-full bg-white text-primary-pink mt-8 mb-4">
+            Continue
+        </button>
+    </div>
+  );
+}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,35 +1,41 @@
+import SocialMediaButton from '~/components/social-media-button';
+import { useState } from 'react';
+import LoginForm from '~/components/login-form';
+import SignupForm from '~/components/signup-form';
+
 export default function Login() {
+  const [authMode, setAuthMode] = useState<'login' | 'signup'>('login');
   return (
-    <div className="bg-white h-dvh text-primary-gray">
-      <div className="flex justify-center items-center h-full gap-4">
+    <div className="bg-white min-h-dvh text-primary-gray flex justify-center items-center">
+      <div className="flex justify-center py-20 md:py-0 items-center h-full md:gap-4">
         <div className="flex flex-col md:flex-row gap-12">
           <div className="flex justify-center items-center flex-col">
             <img src="/GGSTL_LOGO.png" className="w-[300px] md:w-[400px]" />
             <div className="flex gap-4 text-xl md:text-xl font-bold items-center">
-              <h3>Log in</h3>
+              <button
+                className={`px-4 py-2 rounded-full transition-colors duration-700 ease-in-out cursor-pointer ${
+                  authMode === 'login'
+                    ? 'bg-primary-gray text-white shadow-lg'
+                    : ' text-primary-gray'
+                }`}
+                onClick={() => setAuthMode('login')}
+              >
+                Log in
+              </button>
               <div className="border-2 h-[50px]"></div>
-              <h3>Sign up</h3>
+              <button
+                className={`px-4 py-2 rounded-full transition-colors duration-700 ease-in-out cursor-pointer ${
+                  authMode === 'signup'
+                    ? 'bg-primary-gray text-white shadow-lg'
+                    : ' text-primary-gray'
+                }`}
+                onClick={() => setAuthMode('signup')}
+              >
+                Sign up
+              </button>
             </div>
-            <div>
-              <form className="flex flex-col gap-4 mt-4">
-                <input
-                  type="text"
-                  placeholder="Email"
-                  className="px-4 p-2 rounded-full w-[300px] md:w-[400px] shadow-xl h-[50px] outline-none"
-                />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="px-4 p-2 rounded-full w-[300px] md:w-[400px] shadow-xl h-[50px] outline-none"
-                />
-                <button className="hover:bg-gray-800 cursor-pointer bg-primary-gray text-white p-2 rounded-full w-[300px] md:w-[400px] transition">
-                  Log in
-                </button>
-                <button className="hover:bg-gray-800 cursor-pointer bg-primary-gray text-white p-2 rounded-full w-[300px] md:w-[400px] transition">
-                  Forgot password?
-                </button>
-              </form>
-            </div>
+            {authMode === 'login' && <LoginForm />}
+            {authMode === 'signup' && <SignupForm />}
             <div>
               <div className="flex flex-col gap-2 mt-4">
                 <label className="flex items-center gap-2">
@@ -46,7 +52,17 @@ export default function Login() {
             </div>
           </div>
           <div className="border-2 w-full h-1 md:h-130 md:w-1" />
-          <div className="flex justify-center items-center">123123</div>
+          <div className="flex flex-col justify-center items-center">
+            <SocialMediaButton text="Continue with Google">
+              <img src="icons/google.png" height={40} width={40} alt="Google Logo" />
+            </SocialMediaButton>
+            <SocialMediaButton text="Continue with Facebook">
+              <img src="icons/facebook.png" height={40} width={40} alt="Facebook Logo" />
+            </SocialMediaButton>
+            <SocialMediaButton text="Continue with Apple">
+              <img src="icons/apple.png" height={40} width={40} alt="Apple Logo" />
+            </SocialMediaButton>
+          </div>
         </div>
       </div>
     </div>
