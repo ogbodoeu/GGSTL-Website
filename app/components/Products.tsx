50d9afdde6c3b52d9f4ba6f0f29cbbe1da028c9f
ogbodoeu <74887108+ogbodoeu@users.noreply.github.com>
2026-01-09 19:37:19 +0100

    Update Products_5

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -1,25 +1,28 @@
 export default function Products() {
   const products = [
     {
-      title: 'SwiftRoute',
+      title: "SwiftRoute",
+      logoKey: "SwiftRoute",
       description:
-        'Gilead SwiftRoute is an all-in-one platform for secure international money transfers and cheap flight booking ✈️, helping users move money globally and plan travel easily through trusted partners like Trip.com, Wise.com, and more.',
-      url: '/swift-route/decision'
-,
+        "Gilead SwiftRoute is an all-in-one platform for secure international money transfers and cheap flight booking ✈️, helping users find cheap flights, move     money globally, and plan travel easily through trusted partners like trip.com, wise.com, and more.",
+      url: "/swift-route/decision",
     },
     {
-      title: 'SmartMedX',
+      title: "SmartMedX",
+      logoKey: "SmartMedX",
       description:
-        'Gilead SmartMedX integrates artificial intelligence with medical expertise to deliver smarter diagnostics, treatment insights, and healthcare solutions.',
-      url: '/smart-medx',
+        "Gilead SmartMedX integrates artificial intelligence with medical expertise to deliver smarter diagnostics, treatment insights, and healthcare solutions.",
+      url: "/smart-medx",
     },
     {
-      title: 'SmartConnectivitySolutions',
+      title: "Smart Connectivity Solutions & Vehicle Tracking",
+      logoKey: "SmartConnectivitySolutions",
       description:
-        'Gilead Smart Connectivity Solutions and Vehicle Tracking delivers advanced connectivity and communication solutions, offering reliable vehicle tracking installation and comprehensive fleet management.',
-      url: '/smart-connectivity-solutions',
+        "Gilead Smart Connectivity Solutions and Vehicle Tracking delivers advanced connectivity and communication solutions, offering reliable vehicle tracking installation and comprehensive fleet management.",
+      url: "/smart-connectivity-solutions",
     },
   ];
+
   return (
     <div className="flex flex-col sm:flex-row gap-8 w-full p-8 over" id="companies">
       {products.map((product, index) => (
@@ -30,11 +33,13 @@ export default function Products() {
         >
           <div className="p-4 flex flex-col items-center overflow-hidden ">
             <h4 className="text-lg font-semibold mb-2 text-inherit">{product.title}</h4>
+
             <img
-              src={`/images/${product.title}.png`}
+              src={`/images/${product.logoKey}.png`}
               alt={`${product.title} logo`}
               className="mb-2"
             />
+
             <p className="text-sm text-inherit">{product.description}</p>
           </div>
         </a>
@@ -42,3 +47,4 @@ export default function Products() {
     </div>
   );
 }
+
